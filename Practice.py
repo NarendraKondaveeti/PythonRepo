123a4ca3b15ebce76293257c4998636b18763758
Narendra Kondaveeti (ALLYIS INC) <v-narendrako@microsoft.com>
2024-12-22 10:15:57 +0530

    memory

---
+++ Practice.py
@@ -0,0 +1,14 @@
+
+print(0.1+0.2) # 0.30000000000000004
+
+print (3 and 11 and 10)
+
+print (3 and 0 and 10)
+
+print (3 or 1 or 10)
+
+print (3 or 0 or 10)
+
+print (0 or "" or 0.0)
+
+print(2 == 4 == 6)
