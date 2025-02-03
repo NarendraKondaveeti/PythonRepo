e441ccc146ce61a0cbac8ce6d7f7291d2dc2c93d
Narendra Kondaveeti <140384260+NarendraKondaveeti@users.noreply.github.com>
2025-02-03 19:04:08 +0530

    Update Calculate GCD of Two Numbers.py

--- Calculate GCD of Two Numbers.py
+++ Calculate GCD of Two Numbers.py
@@ -1,5 +1,21 @@
 # ====Raw method(it means no ib-built function or methonds)=====
 # Function to find the GCD of two numbers using Euclidean algorithm
+GCD (Greatest Common Divisor) ante rendu numbers ki common ga unna biggest factor (divisor).
+Idi HCF (Highest Common Factor) ani kuda antaru.
+
+📌 Mathematical Definition
+👉 GCD(m, n) = Two numbers ki common ga vache biggest number.
+🔹 GCD(12, 18) Calculation
+✅ 12 divisors → 1, 2, 3, 4, 6, 12
+✅ 18 divisors → 1, 2, 3, 6, 9, 18
+✅ Common divisors → 1, 2, 3, 6
+✅ Biggest common divisor → 6
+
+a % b ni recursive ga calculate chesthu b = 0 ayye varaku loop run avuthundi.
+Final ga a value GCD avutundi.
+
+👉 So, GCD(12, 18) = 6
+
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b  # Swap a and b, and replace b with the remainder of a divided by b
